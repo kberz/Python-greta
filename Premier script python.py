0e3c786874d72f2dae6940a7dd35b7bd89446cb6
kberz <karlbersia2@gmail.com>
2021-03-12 09:26:05 +0100

    Ajout des premiers exercices

---
+++ Premier script python.py
@@ -0,0 +1,13 @@
+#Saisir deux variables entières dans deux variables a et b et affichez leur sommes leurs différences division multiplication
+a = int(input("Entrez une valeur pour a: "))
+b = int(input("Entrez une valeur pour b: "))
+
+a_plus_b = a + b
+a_moins_b = a - b
+a_diviser_b = a / b
+a_multiplier_b = a * b
+
+print("La somme des deux est :" + str(a_plus_b))
+print("La soustraction des deux est :" + str(a_moins_b))
+print("Le produit des deux est :" + str(a_diviser_b))
+print("La multipliaction des deux est :" + str(a_multiplier_b))
---
+++ circonferences.py
@@ -0,0 +1,48 @@
+from math import pi
+#Perimettre de l'aire d'un cercle Perimettre = 2 x Pi x R
+
+r = float(input("Inserre le rayon ici: "))
+
+perimettre_du_cercle = r * 2 * pi
+
+aire_du_cercle = pi * r * r
+
+print("Voici le perimettre: ", perimettre_du_cercle)
+print("Voici l'aire: ", aire_du_cercle)
+
+
+
+
+#Perimettre et aire d'un triangle
+a = float(input("Inserre le premier coté du triangle: "))
+b = float(input("Inserre le deuxième coté du triangle: "))
+c = float(input("Inserre le troisième coté du triangle: "))
+
+perimettre_du_triangle = a + b + c
+
+dp = (a + b + c) / 2
+
+aire_du_triangle = (dp * (dp -a) * (dp - b) * (dp -c))** 0.5
+
+print("L'aire du triangle est: ", aire_du_triangle)
+
+
+#Convertion du degrès en fereninght
+degres = float(input("Insère ici les degrès en c°"))
+
+convertioncf = (degres * 1.8) + 32
+
+print("La conversion en fanhrenheit: ", convertioncf)
+
+
+
+#Convertion heures minutes seconde
+secondes = float(input("insère ici les secondes que tu veux calculer en heures: "))
+
+minutes =  secondes / 60 
+
+heures = minutes / 60
+
+jours = heures / 24
+
+print(" Le resultat est ", jours, "jours", heures,"H", minutes,"m", secondes, "secondes") 
